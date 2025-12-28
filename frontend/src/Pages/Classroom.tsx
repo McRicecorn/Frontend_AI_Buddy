a59423410ef36a891d0df82dd6d2954757546b84
Duc <minhducngo22@yahoo.com>
2025-12-28 19:15:43 +0100

    MediaFenste added

--- frontend/src/Pages/Classroom.tsx
+++ frontend/src/Pages/Classroom.tsx
@@ -4,6 +4,10 @@ import { ChatBoard } from '../components/Chatboard/Chatboard';
 import InputBox from '../components/InputBox/InputBox';
 import type { ChatMessage } from '../Interfaces';
 import StatusBar from '../components/Status/StatusBar';
+import { MediaPanel } from '../components/MediaPanel/MediaPanel';
+import { MediaItem } from '../components/MediaItem/MediaItem';
+import { MediaType } from '../enums/MediaType';
+import './Classroom.css'
 
 interface ClassroomProps {
   username: string;
@@ -11,12 +15,37 @@ interface ClassroomProps {
   onSend: (text: string) => void;
 }
 
+const dummyMedia1 = new MediaItem(
+  '1',
+  MediaType.IMAGE,
+  'Photosynthese',
+  'https://tse1.explicit.bing.net/th/id/OIP.NO4OZ6ocvEJ-jbWsXESazgHaE8?rs=1&pid=ImgDetMain&o=7&rm=3',
+  'Übersicht zur Photosynthese'
+)
+const dummyMedia2 = new MediaItem(
+  '2',
+  MediaType.VIDEO,
+  'Capibara',
+  'https://www.youtube.com/watch?v=8Pj-YEQbojk',
+  'Capibara Song'
+
+)
+let dummyMedia = [dummyMedia1,dummyMedia2]
+
 const Classroom: React.FC<ClassroomProps> = ({ username, messages, onSend }) => {
   return (
-    <div className="classroom-container">
-      <Character />
-      <StatusBar username={username} />
-      <div className="right-side">
+    <div className="classroom">
+      <div className='left'>
+        <Character />
+        <StatusBar username={username} />
+      </div>
+
+      <div className='middle'>
+        <MediaPanel media={dummyMedia}/>
+      </div>  
+
+
+      <div className="right">
         <ChatBoard messages={messages} />
         <InputBox onSend={onSend} />
     
